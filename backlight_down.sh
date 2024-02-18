838db55b8ff3897aa1dadad361e507e0caf68cd8
EnglishmanCom <englishmancom@gmail.com>
2024-02-18 10:36:46 +0100

    Aggiunti configurazione di spectrwm e cheat tasti

---
+++ backlight_down.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+P="/sys/class/backlight/acpi_video0"
+ACT=`more $P/actual_brightness`
+ACT=$((ACT-1))
+echo $ACT >$P/brightness
+# For Lenovo T495 until I will find a better method
+# dont' forget it works with kernel parameter backlight=acpi
